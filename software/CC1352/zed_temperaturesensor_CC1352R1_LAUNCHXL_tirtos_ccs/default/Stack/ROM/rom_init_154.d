243c293fe06201136610340313763f7c544bdc28
Taylor Weil <tcweil@iastate.edu>
2021-10-24 13:21:23 -0500

    added CC1352 projects

---
+++ software/CC1352/zed_temperaturesensor_CC1352R1_LAUNCHXL_tirtos_ccs/default/Stack/ROM/rom_init_154.d
@@ -0,0 +1,42 @@
+# FIXED
+
+Stack/ROM/rom_init_154.obj: ../Stack/ROM/rom_init_154.c
+Stack/ROM/rom_init_154.obj: ../Stack/ROM/rom_jt_def_154.h
+Stack/ROM/rom_init_154.obj: C:/Users/Patrick/workspace_v10/zed_temperaturesensor_CC1352R1_LAUNCHXL_tirtos_ccs/Stack/HAL/Platform/hal_types.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+Stack/ROM/rom_init_154.obj: C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+
+../Stack/ROM/rom_init_154.c:
+
+../Stack/ROM/rom_jt_def_154.h:
+
+C:/Users/Patrick/workspace_v10/zed_temperaturesensor_CC1352R1_LAUNCHXL_tirtos_ccs/Stack/HAL/Platform/hal_types.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1030/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
