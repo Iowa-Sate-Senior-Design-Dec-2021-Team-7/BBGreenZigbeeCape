dfc0d902d0f415dfe0bfa28745764fbd7edc3fca
griffens <griffens@iastate.edu>
2021-12-11 03:26:27 -0600

    - Cleaning more project files :)

--- firmware/SensorTagProject/default/configPkg/package/cfg/app_pem4f.cfg.mak
+++
@@ -1,8 +0,0 @@
-# invoke SourceDir generated makefile for app.pem4f
-app.pem4f: .libraries,app.pem4f
-.libraries,app.pem4f: package/cfg/app_pem4f.xdl
-	$(MAKE) -f /Users/taylorweil/sddec21-07/firmware/SensorTagProject/src/makefile.libs
-
-clean::
-	$(MAKE) -f /Users/taylorweil/sddec21-07/firmware/SensorTagProject/src/makefile.libs clean
-
--- firmware/SensorTagProject/default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := $(GEN_CMDS__FLAG) -l"ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -l"/Users/taylorweil/sddec21-07/firmware/SensorTagProject/default/syscfg/ti_utils_build_linker.cmd.genlibs" -l"/Users/taylorweil/ti/simplelink_cc13x2_26x2_sdk_5_20_00_52/source/ti/ti154stack/library/tirtos/ccs/timac_rom_PG2_0_rom_api_linker.cmd" -l"/Users/taylorweil/ti/simplelink_cc13x2_26x2_sdk_5_20_00_52/source/ti/devices/cc13x2_cc26x2/driverlib/bin/ccs/driverlib.lib" -llibc.a -lti_utils_build_linker.cmd.genlibs
-
